746b673c839d25ae9be604728ccc64b6222d6bdf
DMITRII <dmitremelyanov@yandex.ru>
2014-07-08 14:44:25 +0600

    zadanie1
    zadanie1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace задание_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double a, S, P, L, S_kr;
+
+            Console.Write("Введите сторону квадрата а=");
+            a = Convert.ToDouble(Console.ReadLine());
+            double r = a / 2;
+            double d = a * Math.Sqrt(2);
+
+            S = a * a;
+            P = 4 * a;
+            S_kr = Math.PI * r * r;
+            L = Math.PI * d;
+
+            Console.WriteLine("Площадь квадрата ={0}", S);
+            Console.WriteLine("Периметр квадрата ={0}", P);
+            Console.WriteLine("Площадь круга вписанного в квадрат ={0}", S_kr);
+            Console.WriteLine("Длина окружности вписанная в квадрат ={0}", L);
+            Console.ReadKey();
+        }
+
+    }
+}
